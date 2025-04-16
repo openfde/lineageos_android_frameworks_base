0c827fc78a97478e46227f5a21e9952fb4092691
ironlvxyy <ironmanpeng@126.com>
2025-04-16 10:40:31 +0800

    fixed dialog display incomplete in the application window

--- core/java/android/app/Dialog.java
+++ core/java/android/app/Dialog.java
@@ -69,6 +69,7 @@ import com.android.internal.app.WindowDecorActionBar;
 import com.android.internal.policy.PhoneWindow;
 
 import java.lang.ref.WeakReference;
+import android.util.DisplayMetrics;
 
 /**
  * Base class for Dialogs.
@@ -218,6 +219,9 @@ public class Dialog implements DialogInterface, Window.Callback,
         w.setGravity(Gravity.CENTER);
 
         mListenersHandler = new ListenersHandler(this);
+        if(context instanceof Activity){
+            setOwnerActivity((Activity)context);
+        }
     }
 
     /**
@@ -459,6 +463,19 @@ public class Dialog implements DialogInterface, Window.Callback,
             // Add onBackPressed as default back behavior.
             mDefaultBackCallback = this::onBackPressed;
             getOnBackInvokedDispatcher().registerSystemOnBackInvokedCallback(mDefaultBackCallback);
+        }
+        Window window = getWindow();
+        Log.d(TAG,"onStart window: " + window + ", getOwnerActivity(): " + getOwnerActivity());
+        if (window != null && getOwnerActivity() != null) {
+            DisplayMetrics metrics = getOwnerActivity().getResources().getDisplayMetrics();
+            WindowManager.LayoutParams params = window.getAttributes();
+            Log.d(TAG,"params.width: " + params.width
+                + ", params.height: " + params.height
+                + ", metrics.heightPixels: " + metrics.heightPixels
+                + ", metrics.widthPixels: " + metrics.widthPixels);
+            params.width = Math.min(params.width, metrics.widthPixels - 40);
+            params.height = Math.min(params.height, metrics.heightPixels - 40);
+            window.setAttributes(params);
         }
     }
 
