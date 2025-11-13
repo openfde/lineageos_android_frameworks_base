a1b50a2bc85b0057f624284acfc2217b2d21c3d2
gongyong <185457686@qq.com>
2025-11-13 09:57:44 +0800

    rename to ftg340

--- services/core/java/com/android/server/webkit/SystemImpl.java
+++ services/core/java/com/android/server/webkit/SystemImpl.java
@@ -275,7 +275,7 @@ public class SystemImpl implements SystemInterface {
     @Override
     public boolean isMultiProcessDefaultEnabled() {
         // Multiprocess is enabled by default for all devices.
-        return TextUtils.equals(SystemProperties.get("ro.hardware.egl", "defalut"), "LEOPARD") ? false : true;
+        return TextUtils.equals(SystemProperties.get("ro.hardware.egl", "defalut"), "FTG340") ? false : true;
     }
 
     // flags declaring we want extra info from the package manager for webview providers
