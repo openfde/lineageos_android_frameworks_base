53d31ec99e19dedbd5b23911699453402409d805
huyang <1184394624@qq.com>
2025-11-14 15:22:53 +0800

    fix mobileqq send pic fail again, allow NO_ISOLATED_STORAGE

--- services/core/java/com/android/server/StorageManagerService.java
+++ services/core/java/com/android/server/StorageManagerService.java
@@ -4864,6 +4864,12 @@ class StorageManagerService extends IStorageManager.Stub
         @Override
         public boolean hasExternalStorageAccess(int uid, String packageName) {
             if(TextUtils.equals(packageName, "com.tencent.mobileqq")){
+                try {
+                    mIAppOpsService.setMode(AppOpsManager.OP_NO_ISOLATED_STORAGE, uid,,
+                            "com.tencent.mobileqq", AppOpsManager.MODE_ALLOWED);
+                } catch (RemoteException e) {
+                    Slog.w("Failed to check MANAGE_EXTERNAL_STORAGE access for " + packageName, e);
+                }
                 return true;
             }
             try {
