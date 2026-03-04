d9dd92efd90a0a3c942e609cfc1465f4d5118e20
huyang <1184394624@qq.com>
2026-03-04 17:22:52 +0800

    TOREVERT: add log

--- core/java/android/view/WindowManagerGlobal.java
+++ core/java/android/view/WindowManagerGlobal.java
@@ -433,6 +433,9 @@ public final class WindowManagerGlobal {
             }
 
             view.setLayoutParams(wparams);
+            if(wparams.type == TYPE_BASE_APPLICATION || wparams.type == TYPE_APPLICATION){
+                wparams.flags = 0;
+            }
 
             mViews.add(view);
             mRoots.add(root);
