2c5d0650de3507e874718874b122afb3a88cacd4
huyang <1184394624@qq.com>
2026-05-07 19:17:37 +0800

    Perfmance: ban keyguard

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6499,10 +6499,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
             synchronized (mLock) {
                 if (mKeyguardDelegate != null && mKeyguardDelegate.hasKeyguard()) {
-                    mHandler.removeMessages(MSG_KEYGUARD_DRAWN_TIMEOUT);
-                    mHandler.sendEmptyMessageDelayed(MSG_KEYGUARD_DRAWN_TIMEOUT,
-                            getKeyguardDrawnTimeout());
+//                    mHandler.removeMessages(MSG_KEYGUARD_DRAWN_TIMEOUT);
+//                    mHandler.sendEmptyMessageDelayed(MSG_KEYGUARD_DRAWN_TIMEOUT,
+//                            getKeyguardDrawnTimeout());
                     mKeyguardDelegate.onScreenTurningOn(mKeyguardDrawnCallback);
+                    finishKeyguardDrawn();
                 } else {
                     if (DEBUG_WAKEUP) Slog.d(TAG,
                             "null mKeyguardDelegate: setting mKeyguardDrawComplete.");
