d1808d6f8e9603c84286a760c034bbe2205d0045
huyang <1184394624@qq.com>
2026-05-08 10:23:18 +0800

    Performance: return bindService

--- services/core/java/com/android/server/policy/keyguard/KeyguardServiceDelegate.java
+++ services/core/java/com/android/server/policy/keyguard/KeyguardServiceDelegate.java
@@ -151,19 +151,19 @@ public class KeyguardServiceDelegate {
     }
 
     public void bindService(Context context) {
-        if(true){
-            return;
-        }
-        Intent intent = new Intent();
-        final Resources resources = context.getApplicationContext().getResources();
-
-        final ComponentName keyguardComponent = ComponentName.unflattenFromString(
-                resources.getString(com.android.internal.R.string.config_keyguardComponent));
-        intent.addFlags(Intent.FLAG_DEBUG_TRIAGED_MISSING);
-        intent.setComponent(keyguardComponent);
-
-        if (!context.bindServiceAsUser(intent, mKeyguardConnection,
-                Context.BIND_AUTO_CREATE, mHandler, UserHandle.SYSTEM)) {
+//        if(true){
+//            return;
+//        }
+//        Intent intent = new Intent();
+//        final Resources resources = context.getApplicationContext().getResources();
+//
+//        final ComponentName keyguardComponent = ComponentName.unflattenFromString(
+//                resources.getString(com.android.internal.R.string.config_keyguardComponent));
+//        intent.addFlags(Intent.FLAG_DEBUG_TRIAGED_MISSING);
+//        intent.setComponent(keyguardComponent);
+//
+//        if (!context.bindServiceAsUser(intent, mKeyguardConnection,
+//                Context.BIND_AUTO_CREATE, mHandler, UserHandle.SYSTEM)) {
             Log.v(TAG, "*** Keyguard: can't bind to " + keyguardComponent);
             mKeyguardState.showing = false;
             mKeyguardState.secure = false;
@@ -173,9 +173,9 @@ public class KeyguardServiceDelegate {
                 // stuck on keyguard-less devices.
                 mKeyguardState.deviceHasKeyguard = false;
             }
-        } else {
-            if (DEBUG) Log.v(TAG, "*** Keyguard started");
-        }
+//        } else {
+//            if (DEBUG) Log.v(TAG, "*** Keyguard started");
+//        }
 
         final DreamManagerInternal dreamManager =
                 LocalServices.getService(DreamManagerInternal.class);
