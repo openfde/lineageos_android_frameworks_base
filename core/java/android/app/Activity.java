797ce437e60b2089807fb9ae457bb22eafe06495
huyang <1184394624@qq.com>
2026-05-26 15:56:47 +0800

    TOREVERT: add log

--- core/java/android/app/Activity.java
+++ core/java/android/app/Activity.java
@@ -7780,7 +7780,7 @@ public class Activity extends ContextThemeWrapper
             boolean isSystemApp = isCurrentActivitySystemApp();
 
             Log.d(TAG, "#"+ this +" taskid:" + taskInfo.taskId + " shouldUpdateDecorationStatus: hasOtherActivities:" + hasOtherActivities
-                + " isTransparent:" + isTransparent + " isSystemApp:" = isSystemApp);
+                + " isTransparent:" + isTransparent + " isSystemApp:" + isSystemApp);
             if (isTransparent && hasOtherActivities && isSystemApp) {
                 return false;
             }
