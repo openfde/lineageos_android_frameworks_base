1e5ce06e8a198f5a5f756a01623ff90fda25c655
huyang <1184394624@qq.com>
2026-06-03 15:53:07 +0800

    Performance: add log

--- packages/SystemUI/src/com/android/systemui/SystemUIApplication.java
+++ packages/SystemUI/src/com/android/systemui/SystemUIApplication.java
@@ -162,9 +162,7 @@ public class SystemUIApplication extends Application implements
                     if (mServicesStarted) {
                         final int N = mServices.length;
                         for (int i = 0; i < N; i++) {
-                            if (mServices[i] != null) {
-                                notifyBootCompleted(mServices[i]);
-                            }
+                            notifyBootCompleted(mServices[i]);
                         }
                     }
                 }
@@ -304,35 +302,14 @@ public class SystemUIApplication extends Application implements
                 Class<? extends CoreStartable>[] deps = (dep == null ? null : dep.value());
                 if (deps == null || startedStartables.containsAll(Arrays.asList(deps))) {
                     String clsName = cls.getName();
-                    Log.w("BootOptimize", "startStartable clsName=" + clsName);
                     int i = serviceIndex;  // Copied to make lambda happy.
-                    if (clsName.contains("RingtonePlayer")
-//                    if (clsName.contains("biometrics")
-                            || clsName.contains("PhysicalKeyboardCoreStartable")
-                            || clsName.contains("MediaOutputSwitcherDialogUI")
-                            || clsName.contains("NearbyMediaDevicesManager")
-                            || clsName.contains("StorageNotification")
-                            || clsName.contains("UserSwitcherDialogCoordinator")
-                            || clsName.contains("Keyguard")
-                            || clsName.contains("communal")
-                            || clsName.contains("dreams")
-                            || clsName.contains("recents")
-                            || clsName.contains("biometrics")
-                            || clsName.contains("taptotransfer")
-                            || clsName.contains("notification")
-                            || clsName.contains("VolumeUI")
-//                            || clsName.contains("KeyguardUpdateMonitor")
-                    ) {
-                        Log.w("BootOptimize", "Skipping KeyguardService creation to save time!");
-                    } else {
-                        timeInitialization(
-                                clsName,
-                                () -> mServices[i] = startStartable(clsName, entry.getValue()),
-                                log,
-                                metricsPrefix);
-                        startedStartables.add(cls);
-                        startedAny = true;
-                    }
+                    timeInitialization(
+                            clsName,
+                            () -> mServices[i] = startStartable(clsName, entry.getValue()),
+                            log,
+                            metricsPrefix);
+                    startedStartables.add(cls);
+                    startedAny = true;
                     serviceIndex++;
                 } else {
                     nextQueue.add(entry);
@@ -373,10 +350,6 @@ public class SystemUIApplication extends Application implements
 
         for (serviceIndex = 0; serviceIndex < mServices.length; serviceIndex++) {
             final CoreStartable service = mServices[serviceIndex];
-            if(service == null){
-                continue;
-            }
-
             if (mBootCompleteCache.isBootComplete()) {
                 notifyBootCompleted(service);
             }
@@ -394,9 +367,6 @@ public class SystemUIApplication extends Application implements
     }
 
     private static void notifyBootCompleted(CoreStartable coreStartable) {
-        if(coreStartable == null){
-            return;
-        }
         if (Trace.isEnabled()) {
             Trace.traceBegin(
                     Trace.TRACE_TAG_APP,
@@ -407,7 +377,7 @@ public class SystemUIApplication extends Application implements
     }
 
     private static void timeInitialization(String clsName, Runnable init, TimingsTraceLog log,
-            String metricsPrefix) {
+                                           String metricsPrefix) {
         long ti = System.currentTimeMillis();
         log.traceBegin(metricsPrefix + " " + clsName);
         init.run();
@@ -480,9 +450,7 @@ public class SystemUIApplication extends Application implements
 
             final int N = mServices.length;
             for (int i = 0; i < N; i++) {
-                if(mServices[i] != null){
-                    mServices[i].onConfigurationChange();
-                }
+                mServices[i].onConfigurationChange();
             }
 
             Trace.endSection();
@@ -501,7 +469,7 @@ public class SystemUIApplication extends Application implements
 
     /** Update a notifications application name. */
     public static void overrideNotificationAppName(Context context, Notification.Builder n,
-            boolean system) {
+                                                   boolean system) {
         final Bundle extras = new Bundle();
         String appName = system
                 ? context.getString(com.android.internal.R.string.notification_app_name_system)
