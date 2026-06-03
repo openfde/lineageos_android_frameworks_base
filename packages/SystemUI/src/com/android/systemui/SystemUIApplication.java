58787cc7794cc289f28b3534db1b404c449da4c5
huyang <1184394624@qq.com>
2026-06-03 18:34:42 +0800

    Performance: add log

--- packages/SystemUI/src/com/android/systemui/SystemUIApplication.java
+++ packages/SystemUI/src/com/android/systemui/SystemUIApplication.java
@@ -313,7 +313,7 @@ public class SystemUIApplication extends Application implements
                             || clsName.contains("NearbyMediaDevicesManager")
                             || clsName.contains("StorageNotification")
                             || clsName.contains("UserSwitcherDialogCoordinator")
-//                            || clsName.contains("Keyguard")
+                            || clsName.contains("keyguard")
                             || clsName.contains("KeyguardViewConfigurator")
                             || clsName.contains("communal")
                             || clsName.contains("dreams")
