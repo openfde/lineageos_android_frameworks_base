717373fb9004a5444c72d59c12ec80133675f0a5
huyang <1184394624@qq.com>
2026-06-08 15:51:20 +0800

    Performance: fix npe install CaptionWindowDecorViewModel

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -1151,7 +1151,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
                     @Override
                     public void onPluginConnected(OverlayPlugin plugin, Context pluginContext) {
                         Log.w(TAG, "onPluginConnected() called with: plugin = [" + plugin + "], QSTileImpl.handler = [" + QSTileImpl.handler + "]", new Throwable());
-                        if (QSTileImpl.handler == null && mStatusBarView != null) {
+                        if (QSTileImpl.handler == null || mStatusBarView == null) {
                             mHandler.removeMessages(MSG_PLUGIN_SETUP);
                             Message msg = Message.obtain();
                             msg.what = MSG_PLUGIN_SETUP;
