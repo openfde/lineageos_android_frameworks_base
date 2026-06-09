6f255c30499f363eff87060611784653c95f7578
huyang <1184394624@qq.com>
2026-06-09 14:32:46 +0800

    Performance: optmize boringdroid.systemui startup
    opt setup

--- packages/SystemUI/src/com/android/systemui/statusbar/StatusBarIconView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/StatusBarIconView.java
@@ -444,7 +444,7 @@ public class StatusBarIconView extends AnimatedImageView implements StatusIconDi
         if (mIcon == null) {
             return false;
         }
-        Drawable drawable;
+        Drawable drawable == null;
         try {
             Trace.beginSection("StatusBarIconView#updateDrawable()");
 //            drawable = getIcon(mIcon);
